38afe07e59cc0df026de0866f240598f9ca1997a
fy <fy0@qq.com>
2017-03-22 22:53:37 +0800

    准备加tag

--- backend/model/models.py
+++ backend/model/models.py
@@ -4,6 +4,7 @@ from model import db
 from model.user import User
 from model.topic import Topic
 from model.comment import Comment
+from model.tag import Tag, TopicTag
 
 db.connect()
-db.create_tables([User, Topic, Comment], safe=True)
+db.create_tables([User, Topic, Comment, Tag, TopicTag], safe=True)
---
+++ backend/model/tag.py
@@ -0,0 +1,23 @@
+﻿# coding:utf-8
+
+from peewee import *
+from model import BaseModel
+
+
+class Tag(BaseModel):
+    name = CharField(index=True, unique=True, max_length=40)
+    time = BigIntegerField(max_length=40)
+    desc = CharField(max_length=255)
+
+    class Meta:
+        db_table = 'tag'
+
+
+class TopicTag(BaseModel):
+    topic_id = BigIntegerField(index=True)
+    tag_id = BigIntegerField(index=True)
+    user_id = BigIntegerField(index=True)
+    time = BigIntegerField(index=True)
+
+    class Meta:
+        db_table = 'topic_tag'
