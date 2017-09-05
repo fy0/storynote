7e831414c83d14a81375941d723b7696672843be
fy <fy0@qq.com>
2017-09-06 02:16:54 +0800

    fix

--- page/src/config.js
+++ page/src/config.js
@@ -13,6 +13,7 @@ let config = {
 try {
     let pri = require("../private.js")
     config.remote.API_SERVER = pri.default.remote.API_SERVER || config.remote.API_SERVER;
+    config.title = pri.default.title || config.title;
 } catch (e) {}
 
 export default config;
