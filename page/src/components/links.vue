010015ba0ebc2021994bc0da2dc7c6130bd2eb30
fy <fy0@qq.com>
2017-09-15 15:34:46 +0800

    fix

---
+++ page/src/components/links.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <h4>友情链接</h4>
+    <h5>个人博客</h5>
+    <ul>
+        <li>暂无</li>
+    </ul>
+    <h5>技术社区</h5>
+    <ul>
+        <li><a href="//www.codedata.cn" target="_blank">Code Data</a></li>
+    </ul>
+</div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+    data () {
+        return {
+        }
+    }
+}
+</script>
