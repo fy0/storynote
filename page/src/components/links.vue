16173fad75ab972be86319628b6e837c7219738f
fy <fy0@qq.com>
2017-12-05 02:24:04 +0800

    增加友链

--- page/src/components/links.vue
+++ page/src/components/links.vue
@@ -3,7 +3,7 @@
     <h3>友情链接</h3>
     <h5>个人博客</h5>
     <ul>
-        <li>暂无</li>
+        <li><a href="http://manjusaka.itscoder.com" target="_blank">一只哈士奇</a></li>
     </ul>
     <h5>技术社区</h5>
     <ul>
